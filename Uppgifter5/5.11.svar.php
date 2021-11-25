fd4a261e3cbae3c158a7b460df56bf7e4ebe5f63
AzureAD\TildeFagerström <fagerstromtilde@gmail.com>
2021-11-25 14:51:58 +0200

    Lagt till 5.11.svar.php

---
+++ Uppgifter5/5.11.svar.php
@@ -0,0 +1,16 @@
+<?php
+
+$namn=$_POST['namn'];
+$fodelsedag=$_POST['fodelsedag'];
+$user=$_POST['user'];
+$losen=$_POST['losen'];
+$telefon=$_POST['telefon'];
+
+echo "Du skrev in följande:<br>";
+echo "Namn: <b>$namn</b><br>";
+echo "Födelsedag: <b>$fodelsedag</b><br>";
+echo "Användarnamn: <b>$user</b><br>";
+echo "Lösenord: <b>$losen</b><br>";
+echo "Telefon: <b>$telefon</b><br>";
+
+
